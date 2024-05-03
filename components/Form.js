e4273c80dd7e811b8a8a2788cc0eaa967aa578c9
MD Naem <mmnayeem408@gmail.com>
2024-05-04 00:37:54 +0600

    fron back btn add

--- components/Form.js
+++ components/Form.js
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import bacBtn from "@/public/back-arrow.svg";
+import Link from "next/link";
+
 
 
 export default function Form() {
     return (
         <div className=' py-10'>
-            <div></div>
+            <Link href='/'><Image className="text-white w-6" src={bacBtn}></Image></Link>
             <div id="ff-compose"></div>
             <script async defer src="https://formfacade.com/include/115790938280652452742/form/1FAIpQLSe1jDjRnBD_de15BXd1cZ9Oi5cdaQ2RjmdF-VRkFZMyb1wJrQ/classic.js?div=ff-compose"></script>
 
